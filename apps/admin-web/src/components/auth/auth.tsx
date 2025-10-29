cf1628537b31a742c1357625010dedd0cdcbb4c5
case <1969954161@qq.com>
2025-10-29 19:47:46 +0800

    feat: auth

---
+++ apps/admin-web/src/components/auth/auth.tsx
@@ -0,0 +1,16 @@
+import { useUserStore } from '@/stores/user';
+import { Navigate } from '@modern-js/runtime/router';
+import type { PropsWithChildren } from 'react';
+import type { FC } from 'react';
+
+const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+  const userStore = useUserStore();
+
+  if (!userStore.isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default AuthProvider;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './galaxy';
 export * from './liquid-ether';
 export * from './splash-cursor';
+export * from './blur-text';
