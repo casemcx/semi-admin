aed189c9cf34b66275e9102eb56032add24b2de5
case <1969954161@qq.com>
2025-12-05 16:45:06 +0800

    fix: components构建

--- packages/components/rslib.config.ts
+++ packages/components/rslib.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   source: {
     entry: {
-      index: ['./src/**/*.{ts,tsx}'],
+      index: ['./src/**/*.{ts,tsx,less}'],
     },
   },
   lib: [
