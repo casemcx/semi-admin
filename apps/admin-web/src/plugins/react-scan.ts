442eddb1d7ab0373e4b703e7ef928a8a35ffaeb6
case <1969954161@qq.com>
2025-12-05 17:19:31 +0800

    feat: scan

--- apps/admin-web/src/plugins/react-scan.ts
+++ apps/admin-web/src/plugins/react-scan.ts
@@ -1,14 +1,14 @@
 import type { RuntimePluginFuture } from '@modern-js/runtime';
-// import { scan } from 'react-scan';
+import { scan } from 'react-scan';
 
 const reactScanPlugin: RuntimePluginFuture = {
   name: 'react-scan',
   setup(app) {
     app.onBeforeRender(() => {
       console.log('react-scan');
-      //   scan({
-      //     enabled: true,
-      //   });
+      scan({
+        enabled: true,
+      });
     });
   },
 };
